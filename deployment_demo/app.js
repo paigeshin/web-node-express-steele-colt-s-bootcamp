7caeebb32770271fd80e323f5ad2a9d2fbeea0be
shin <proscus0706@gmail.com>
2019-05-28 08:02:55 +0900

    update 190528 am

--- deployment_demo/app.js
+++ deployment_demo/app.js
@@ -11,6 +11,6 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.listen(3000, "127.0.0.1", function() {
+app.listen(process.env.PORT, process.env.IP, function() {
   console.log("Server has started");
 });
